b8ef5678e9edbc7704cebfc2d5087c1c12ef48ff
Yash_Shhhhh <66167011+yash1sh@users.noreply.github.com>
2022-08-23 10:04:27 +0530

    Add files via upload
    added new file

---
+++ Narrowing.java
@@ -0,0 +1,10 @@
+class Narrowing
+{
+	public static void main(String args[])
+{
+	int d=(short)4.8d;
+	int d1=(int)(short)(int)(float)(long)5.8d;
+	System.out.println(d);
+	System.out.println(d1);
+}
+}
