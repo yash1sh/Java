bc122a4eb4d50c977fc5f9b53a09a59c1d5fa4ad
Yash_Shhhhh <66167011+yash1sh@users.noreply.github.com>
2022-08-24 10:33:07 +0530

    Add files via upload
    added file

---
+++ lab.java
@@ -0,0 +1,9 @@
+public class lab {
+    public static void main(String[] args) {
+        int money=88849384;
+        String car="lamborgini";
+        if(money==88849384 && car.equals("lamborgini"))
+        System.out.println("raje can marry sheela");
+        
+    }
+}
