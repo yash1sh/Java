ea2cd1f77abb95b25314af3b6bd3cf749c1707c9
Yash_Shhhhh <66167011+yash1sh@users.noreply.github.com>
2022-08-29 14:14:41 +0530

    Add files via upload

---
+++ pro43user.java
@@ -0,0 +1,12 @@
+public class pro43user {
+    String username;
+    String email;
+    int pwd;
+    pro43user(String username,String email,int pwd)
+    {
+        this.username=username;
+        this.email=email;
+        this.pwd=pwd;
+    }
+    
+}
