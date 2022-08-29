c45c13d3d7ee394465e8aa45d6f719ba310e6070
Yash_Shhhhh <66167011+yash1sh@users.noreply.github.com>
2022-08-29 14:14:31 +0530

    Add files via upload

---
+++ program39.java
@@ -0,0 +1,15 @@
+public class program39 {
+    static{
+        System.out.println("mlsi 1st executed");
+
+    }
+    static{
+        System.out.println("mlsi 2nd executed");
+
+    }
+    static int b=20;
+    static boolean b1;
+    public static void main(String[] args) {
+        System.out.println(b1);
+    }
+}
