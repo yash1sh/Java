a3f809160d2fd88155ae1f35045b450bda76647d
Yash_Shhhhh <66167011+yash1sh@users.noreply.github.com>
2022-08-30 10:06:00 +0530

    Add files via upload

---
+++ pro48whatsapp.java
@@ -0,0 +1,10 @@
+public class pro48whatsapp {
+    void msg()
+    {
+        System.out.println("you can send a message");
+    }
+    public static void main(String[] args) {
+        pro48whatsapp p= new pro48whatsapp2();
+        p.msg();
+    }
+}
---
+++ pro48whatsapp2.java
@@ -0,0 +1,6 @@
+public class pro48whatsapp2 extends pro48whatsapp {
+    public void msg()
+    {
+        System.out.println("you can callll");
+    }
+}
