7b4535668ae35aa7ef1ed7b3af3540f3f3e673b9
Yash_Shhhhh <66167011+yash1sh@users.noreply.github.com>
2022-09-01 11:43:35 +0530

    Add files via upload
    new java files

---
+++ pro57pen.java
@@ -0,0 +1,11 @@
+public abstract class pro57pen {
+    pro57pen()
+    {
+        System.out.println("pen created");
+    }
+    abstract public void pro57pen();
+    void display()
+    {
+        System.out.println("pem is used");
+    }
+}
---
+++ pro57rose.java
@@ -0,0 +1,12 @@
+import java.time.chrono.MinguoChronology;
+
+public class pro57rose extends pro57pen{
+    public void pro57pen()
+    {
+        System.out.println("Hiiiii");
+    }
+     public static void main(String[] args) {
+        pro57pen p= new pro57rose();
+        p.pro57pen();
+    }
+}
