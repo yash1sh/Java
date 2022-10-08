f2a75724725fb03125a28e3100b4bbe323c5eb43
YajneshVishwakarma <75421738+YajneshVishwakarma@users.noreply.github.com>
2022-10-08 23:32:56 +0530

    Update myntra.java

--- myntra.java
+++ myntra.java
@@ -4,6 +4,7 @@ public class myntra extends Shopping{
     
         myntra(int belt,int shoes,int dress)
         {
+            //Super key word used to access superclass constructor.
             super(belt, shoes, dress);
         }
         public void getresult()
